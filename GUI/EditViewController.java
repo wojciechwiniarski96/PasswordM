54975cfee82bb83cc003dee9e42f2b0d121478d2
wojciechwiniarski96 <wojciechwiniarski96@gmail.com>
2021-01-23 15:08:26 +0100

    Added posibility to edit your password.

--- GUI/EditViewController.java
+++ GUI/EditViewController.java
@@ -2,25 +2,30 @@ package GUI;
 
 import User.User;
 import database.DataBaseManager;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class EditViewController implements Initializable {
 
     @FXML
-    private TextField usernameTextfield;
-    @FXML
-    private TextField passwordTextfield;
-    @FXML
-    private TextField emailTextfield;
+    private TextField passwordTextField;
     @FXML
     private Button confirmationButton;
     @FXML
@@ -28,12 +33,12 @@ public class EditViewController implements Initializable {
 
     private DataBaseManager dataBaseManager;
     private Connection connection;
-    private Statement statement;
 
     private String username;
     private String password; //TODO: encryption
     private String email;
 
+    private int id;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -41,31 +46,66 @@ public class EditViewController implements Initializable {
         dataBaseManager = new DataBaseManager();
         editInput();
         cancel();
+
     }
 
     public void editInput(){
 
+
         confirmationButton.setOnAction(event -> {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            User u = (User)stage.getUserData();
 
-           username = usernameTextfield.getText();
-           password = passwordTextfield.getText();
-           email = emailTextfield.getText();
+            connection = dataBaseManager.connectToDB();
 
+            String newPassword = passwordTextField.getText();
 
-           connection = dataBaseManager.connectToDB();
+            username = u.getUsername();
+            System.out.println("Username  " + username);
+            System.out.println("New password " + newPassword);
 
-           UserViewController userViewController = new UserViewController();
-           userViewController
+            if(!passwordTextField.getText().equals("")){
+                String updateUsername = "UPDATE gPOZ3L2sft.users " +
+                        "SET users.main_password = '" + newPassword + "' " +
+                        "WHERE main_username = '" + username + "';";
 
-           String queryEdit = "";
-
+                try {
+                    PreparedStatement preparedStatement = connection.prepareStatement(updateUsername);
+                    preparedStatement.execute();
+                    System.out.println("Executing 'UPDATE' query ended successfully");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }finally{
+                    try{
+                        Statement statement = connection.createStatement();
+                        if(statement != null){
+                            statement.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try{
+                        if(connection != null){
+                            connection.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         });
     }
+
 
     public void cancel(){
 
         cancelButton.setOnAction(event -> ((Node)(event.getSource())).getScene().getWindow().hide());
     }
+//
+//    public void setUsername(String username){
+//        this.username = username;
+//    }
 
 
 }
