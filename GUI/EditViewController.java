6bc2e829831e71769048af4d08a8fd87f008a6e5
wojciechwiniarski96 <wojciechwiniarski96@gmail.com>
2021-01-30 20:07:36 +0100

    After editing user input the scene will be reloaded

--- GUI/EditViewController.java
+++ GUI/EditViewController.java
@@ -2,17 +2,15 @@ package GUI;
 
 import User.User;
 import database.DataBaseManager;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 
 import java.io.IOException;
 import java.net.URL;
@@ -50,8 +48,6 @@ public class EditViewController implements Initializable {
     }
 
     public void editInput(){
-
-
         confirmationButton.setOnAction(event -> {
             Node node = (Node) event.getSource();
             Stage stage = (Stage) node.getScene().getWindow();
@@ -94,6 +90,17 @@ public class EditViewController implements Initializable {
                     }
                 }
             }
+            //TODO: ALERT FOR USER ABOUT EDITING THE INPUT
+            stage.close();
+            try{
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/UserView.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
     }
 
@@ -107,5 +114,5 @@ public class EditViewController implements Initializable {
 //        this.username = username;
 //    }
 
-
+//TODO: Password Manager not Database Manager
 }
