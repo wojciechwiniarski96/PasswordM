0452f34880d84eda8cbd68e478003f05a26e5470
wojciechwiniarski96 <wojciechwiniarski96@gmail.com>
2021-01-30 20:04:55 +0100

    Added functionality for cancel button; UserView Scene will be refreshed (or reloaded) after adding new content

--- GUI/AddContentViewController.java
+++ GUI/AddContentViewController.java
@@ -2,10 +2,16 @@ package GUI;
 
 import database.DataBaseManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,8 +30,7 @@ public class AddContentViewController implements Initializable {
     @FXML
     private Button enterButton;
     @FXML
-    private Button buttonCancel;
-
+    private Button cancelButton;
 
     private DataBaseManager dataBaseManager;
     private Connection connection;
@@ -38,7 +43,7 @@ public class AddContentViewController implements Initializable {
         insertNewEntry();
     }
 
-    public void insertNewEntry(){
+    private void insertNewEntry(){
 
         enterButton.setOnAction(event -> {
             String username = usernameTextField.getText();
@@ -50,7 +55,7 @@ public class AddContentViewController implements Initializable {
             try {
                 connection = dataBaseManager.connectToDB();
 
-                String queryUseTable = "USE testdatebase;";
+                String queryUseTable = "USE gPOZ3L2sft;";
                 String queryInsert = "INSERT INTO users(main_username, main_password, url) " +
                         "VALUES('" + username + "', '" + password + "', " + "'" + website + "');";
 
@@ -94,13 +99,39 @@ public class AddContentViewController implements Initializable {
                 }
             }
 
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.close();
+            try{
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/UserView.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
 
     }
 
-    public void cancel(){
+    private void cancel(){
 
-        buttonCancel.setOnAction(event -> ((Node)(event.getSource())).getScene().getWindow().hide());
+        cancelButton.setOnAction(event -> {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.close();
+            try{
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/UserView.fxml"));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
     }
 
     //https://www.w3schools.com/sql/sql_insert_into_select.asp
